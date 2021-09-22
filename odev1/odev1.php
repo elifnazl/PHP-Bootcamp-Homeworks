d2fb5f7a89062e427ab4604875a1de8dc85f03bd
Elif Nazlı <eayhan@marun.edu.tr>
2021-09-22 14:20:18 +0300

    Create odev1.php

---
+++ odev1/odev1.php
@@ -0,0 +1,14 @@
+<?php
+
+$age = 29;
+if ($age >= 0 && $age <= 14) {   // 0-14 = Çocuk
+   echo "cocuk";
+} elseif ( $age > 14 && $age <= 24) { // 15-24 = Genç
+    echo "genc";
+} elseif ($age > 24  && $age <= 64) { // 25-64 = Yetişkin
+    echo "yetiskin";
+} else { // 65+ = Yaşlı
+    echo "yaslı";
+}
+  
+ //age degiskeni 29 oldugu icin ekran cıktısı 'yetiskin' olacak.
