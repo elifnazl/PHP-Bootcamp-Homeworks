d21be7d70e7b003da76f992610ee4df68275ef92
Elif Nazlı <eayhan@marun.edu.tr>
2021-10-05 06:53:28 +0300

    Update posts.php

--- odev3/posts.php
+++ odev3/posts.php
@@ -22,41 +22,15 @@
 
 /************************** ODEV **************************/
 
-$server=basename($_SERVER['REQUEST_URI']);
-$file=basename(__FILE__);
-   
-if($server!=$file) {
-    function getLatestPosts($count = 5){
-        $posts = [];
-        $postTypes = ["urgent", "warning", "normal"];
-        for($i=1; $i<=$count; $i++) {
-            do {
-                $id = rand(1, 1000);
-            }
-            while (array_key_exists($id, $posts));
-            $type = $postTypes[rand(0, count($postTypes)-1)];
-            $posts[$id] = [
-            "title" => "Yazı " . $i,
-            "type" => $type
-        ];
-    }
-    return $posts;
-}
-function getPostDetails($id, $title){
-    echo "<h1>".$title." (#".$id.")</h1>";
-    echo <<<EOT
-    <p>
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin a iaculis justo, ac molestie justo. Integer semper nibh non imperdiet blandit. Integer nec diam eget sapien viverra rutrum ut eu justo. Suspendisse efficitur pretium eleifend. Vivamus ex nibh, euismod eget massa ut, accumsan ullamcorper nisi. Phasellus tristique magna et nibh dictum rhoncus. Phasellus at metus quis mi egestas blandit. Vestibulum lacinia ut tortor nec consectetur. Nulla sed risus ut est imperdiet vulputate ac non quam. Aliquam viverra erat vitae diam commodo, et molestie metus ultricies. Praesent rutrum urna a nisi egestas aliquam sit amet eu eros.
-    </p>
-    EOT;
-}   
-function getRandomPostCount($mindeg, $maxdeg){
-    return rand($mindeg,$maxdeg);
-}
-else {
-   echo "403 Forbidden";
-   exit();
+$script="posts.php";
+require "function.php";
+$randomCou t=getRandomPostCount(min:3,max:9);
+$posts=getLatestPostCount($randomCount); 
 
-}  
+foreach($posts as $id=> $post){
+   $title=$post ['title'];
+   $type=$post[ 'type'];
+   include "post.php";
+}
 
-// Aşağıya fonksiyonu tanımlayabilirsiniz.
+
